ee7d5863e950f8b1ec07f9eaf9686f8eb1b112be
Aaditya Singh <aadityasingh9601@gmail.com>
2026-03-07 15:26:18 +0000

    property decorators, getters & setters & operator overloading

--- oops/file2.py
+++ oops/file2.py
@@ -8,3 +8,44 @@ class Person:
 p = Person()
 p.a = 33
 p.show()
+
+class Employee:
+    b = 33
+    
+    # The method name with ‘@property’ decorator is called getter method. 
+    @property
+    def name(self):
+        return f"{self.fname} {self.lname}"
+    # Setter method to update the property.
+    @name.setter
+    def name(self,value):
+        self.fname = value.split(" ")[0]
+        self.lname = value.split(" ")[1]
+
+
+empyloyee = Employee()
+print(empyloyee.b)
+empyloyee.name = "John Doe"
+print(empyloyee.fname,empyloyee.lname)
+
+
+# Operator overloading in Python.
+class Number:
+    def __init__(self,n):
+        self.n = n
+    
+    # Overloading the functionality of add.
+    def __add__(self,num):
+        return self.n + num.n
+    
+    # Multiplication
+    def __mul__(self, num):
+        return self.n * num.n
+    
+    # Similarly we can overload other dunder methods functionality too.
+
+a = Number(1)
+b = Number(2)
+
+print(a+b)
+print(a * b)
